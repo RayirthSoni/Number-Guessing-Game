b91e15d3cc540e35462ff641b5c57daaebdb8d6e
Rayirth Soni <rayirthsoni04@gmail.com>
2024-10-26 14:02:48 +0530

    update: liniting constants

--- configs/constants.py
+++ configs/constants.py
@@ -1,20 +1,18 @@
-'''
+"""
 Constants
-'''
+"""
 
 
 class Constants:
-    '''
+    """
     Constants configurations
-    '''
+    """
+
     class Difficulty:
-        '''
+        """
         Difficulty configurations
-        '''
-        possible_difficulties = ['easy', 'medium', 'hard']
-        default_difficulty = 'medium'
-        number_range = {
-            'easy': (1, 10),
-            'medium': (1, 100),
-            'hard': (1, 1000)
-        }
+        """
+
+        possible_difficulties = ["easy", "medium", "hard"]
+        default_difficulty = "medium"
+        number_range = {"easy": (1, 10), "medium": (1, 100), "hard": (1, 1000)}
